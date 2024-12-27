42cddde6d1b36a60727f13aa0cbdd96bacf3fa47
SMHRD <SMHRD@192.168.219.103>
2024-12-27 09:57:00 +0900

    css 커밋

--- sroup/src/main/java/com/smhrd/sroup/controller/MainController.java
+++ sroup/src/main/java/com/smhrd/sroup/controller/MainController.java
@@ -1,8 +1,14 @@
 package com.smhrd.sroup.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "history";
+	}
 	
 }
