9f4b17fd8a6eaa5453df330f1916e171f379bdbb
노이린 <lapis3597@gmail.com>
2024-12-28 14:26:20 +0900

    1. 화면 전환 가능: 홈 -> 로그인 화면, 홈 -> 회원가입 화면
    2. 1과 관련하여 3-1.main-out.html의 button 태그 수정

--- sroup/src/main/java/com/smhrd/sroup/controller/MainController.java
+++ sroup/src/main/java/com/smhrd/sroup/controller/MainController.java
@@ -3,34 +3,37 @@ package com.smhrd.sroup.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smhrd.sroup.entity.UserEntity;
+import com.smhrd.sroup.model.UserVO;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
-	
+
 	@GetMapping("/")
-	public String home() {
-		return "3-1.main-out";
+	public String home(@RequestParam(value = "action", required = false) String action) {
+
+		if ("login".equals(action)) {
+			// 로그인 페이지로 이동
+			return "2.login"; // 2.login.html 파일 반환
+		} else if ("signup".equals(action)) {
+			return "1.signup"; // 1.signup.html 파일 반환
+		}
+
+		// 기본적으로 메인 화면 반환
+		return "3-1.main-out"; // 3-1.main-out.html 파일 반환
 	}
-	
+
 	@PostMapping("/history.html")
 	public String history() {
 		return "history";
 	}
-	
+
 	@PostMapping("/mystudy.html")
 	public String study() {
 		return "mystudy";
-	}
-	
-	//로그인 기능
-	@PostMapping("/login.html")
-	public void login(String user_id, String user_pw, HttpSession session) {
-		System.out.println("user_id :" + user_id);
-		
-		UserEntity enti = repo.findBy
-		
-		
 	}
 }
