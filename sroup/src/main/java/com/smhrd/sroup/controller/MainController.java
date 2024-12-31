445dd20d4da0bc7813bcc4bac9f8a96913158dbb
SMHRD <SMHRD@192.168.219.141>
2024-12-31 10:34:58 +0900

    Maincontroller 수정 - 은정 커밋

--- sroup/src/main/java/com/smhrd/sroup/controller/MainController.java
+++ sroup/src/main/java/com/smhrd/sroup/controller/MainController.java
@@ -124,7 +124,7 @@ public class MainController {
         return "로그인기능";
     }
 	
-		
+}	
 		
 
 	// 화면 전환 GET 방식 end ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
@@ -132,29 +132,3 @@ public class MainController {
 	
 	
 	// 화면 전환 POST 방식 start ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-	// 로그인 기능
-	@PostMapping("/login.do")
-	public String login_do(@RequestParam ("user_id") String userId, 
-			@RequestParam ("user_pw") String userPw, HttpSession session, Model model) {
-		UserVO userVO = new UserVO();
-		userVO.setUser_id(userId);
-		userVO.setUser_pw(userPw);
-		
-		UserVO vo = userMapper.login(userVO);
-		
-		System.out.println(vo.getUser_id());
-	    // 로그인 성공 여부 확인
-	    if (vo != null) {
-	    	// 세션에 사용자 정보 저장
-	        session.setAttribute("user", vo);
-	        return "3.main-in";		// 로그인 성공 후 3.main-in.html로 이동 
-	        //return "redirect:/"; // 로그인 성공 후 메인 페이지로 리다이렉트
-	    } else {
-	    	// 로그인 실패 시 에러 메세지 전달
-	        model.addAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다.");
-	        return "2.login"; // 로그인 페이지로 다시 이동
-	    }
-
-		 //return "redirect:/";
-		}
-}
